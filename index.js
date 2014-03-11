717752026d90f0129a55c0564df70f0e4c8caed2
E.Azer Koçulu <azer@kodfabrik.com>
2014-03-10 19:42:36 -0700

    guard undefined

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var homedir = process.env[(process.platform == 'win32') ? 'USERPROFILE' : 'HOME'
 module.exports = expandHomeDir;
 
 function expandHomeDir (path) {
+  if (!path) return path;
   if (path == '~') return homedir;
   if (path.slice(0, 2) != '~/') return path;
   return join(homedir, path.slice(2));
